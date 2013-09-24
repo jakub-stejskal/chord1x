b89ff575abbb1efae1b790ea68c6c7208d860108
kub1x <j.podlaha@gmail.com>
2013-09-24 06:31:45 -0700

    rails app init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chord1x::Application.config.secret_key_base = 'a900b89d66b1aaf0a947412cc31f8cf123df516a990fe8aaaccc1a063806701a22ae61b42a06d2a0d528986a76e622a8cd056932271b54d9a1b7aaececf4b54f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chord1x::Application.config.session_store :cookie_store, key: '_chord1x_session'
